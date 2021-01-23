63bcb02711acc9b9875c8933d366633873def734
smaran7713 <smaranmipl@gmail.com>
2021-01-23 12:56:09 +0600

    assignment

---
+++ assignment.js
@@ -0,0 +1,31 @@
+
+kilometerToMeter
+
+function kilometerToMeter(kilometer){
+    var meter = kilometer * 1000;
+    return meter;
+}
+var tangailDistance= kilometerToMeter(40);
+console.log(tangailDistance);
+
+budgetCalculator
+
+function budgetCalculator(watch, mobile, laptop){
+   var result = (watch * 15) + (mobile * 10) + (laptop * 5);
+   return result;  
+}
+var sum = budgetCalculator(50, 100, 500);
+console.log(sum);
+
+hotelCost
+
+function hotelCost(firststep, secoundstep, thirdstep){
+    var result = (firststep * 10) + (secoundstep * 10) + (thirdstep * 10);
+    return result;  
+ }
+ var sum = hotelCost(100, 80, 50);
+ console.log(sum);
+
+megaFriend
+ 
+megaFriend = ['Ruma', 'Sima', 'Salma','Puma'];
